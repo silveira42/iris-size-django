adbe1b36188e3835c801e37eae9f40d164bd13c4
heloisatambara <heloisatambara@usp.br>
2023-05-19 12:04:47 -0300

    starting home page and urls

--- globals/views.py
+++ globals/views.py
@@ -1,3 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Global
 
 # Create your views here.
+def home(request):
+    return render(request, "index.html")
