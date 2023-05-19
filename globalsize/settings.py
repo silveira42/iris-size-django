d5c5d0b72e248ca85edeb25dbf65659ef8658159
heloisatambara <heloisatambara@usp.br>
2023-05-19 11:52:50 -0300

    add iris

--- globalsize/settings.py
+++ globalsize/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'globalsize.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django_iris',
+        'NAME': 'USER',
+        'USER': '_system',
+        'PASSWORD':'SYS',
+        'HOST': 'localhost',
+        'PORT':1972,
     }
 }
 
