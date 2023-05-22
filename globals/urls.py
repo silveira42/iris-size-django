fd6ab4b6251ce486f907226258353b2cc433e4c0
heloisatambara <heloisatambara@usp.br>
2023-05-22 12:23:42 -0300

    filter by database

--- globals/urls.py
+++ globals/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from .views import home, update
 
 urlpatterns = [
-    path('', home),
+    path('', home, name="home"),
     path('update', update, name="update"),
 ]
--- globals/views.py
+++ globals/views.py
@@ -5,14 +5,25 @@ from .api.methods import *
 # Create your views here.
 def home(request):
     iglobals = iGlobal.objects.all()
-    print(iglobals)
-    return render(request, "index.html", {"iglobals": iglobals})
+
+    # handle filters
+    database = request.GET.get("database")#, request.GET.get("fglobal")
+
+    if database:# or fglobal:
+        iglobals = iglobals.filter(database__icontains=database)#.filter(name__icontains=fglobal)
+    else:
+        database=""
+
+    return render(request, "index.html", {"iglobals": iglobals, "database": database})#, "fglobal":fglobal})
+
 
 def update(request):
-    iGlobal.objects.all().delete() #TODO: CHANGE THIS TO A PROPER UPDATE
+    iGlobal.objects.all().delete()
     databaseList = getAllDatabaseDirectories()
+
     for database in databaseList:
         globalList = getGlobalsList(database)
+
         for glob in globalList:
             used, allocated = getGlobalSize(database, glob)
             iGlobal.objects.create(database=database, name=glob, realsize=used, allocatedsize=allocated)
