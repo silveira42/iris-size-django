3b42037801eb55721e26cb197203928e790cf2cd
heloisatambara <heloisatambara@usp.br>
2023-05-22 13:23:55 -0300

    filter by global name and size

--- globals/views.py
+++ globals/views.py
@@ -7,14 +7,20 @@ def home(request):
     iglobals = iGlobal.objects.all()
 
     # handle filters
-    database = request.GET.get("database")#, request.GET.get("fglobal")
+    fdatabase, fglobal, fsize = request.GET.get("fdatabase"), request.GET.get("fglobal"), request.GET.get("fsize")
 
-    if database:# or fglobal:
-        iglobals = iglobals.filter(database__icontains=database)#.filter(name__icontains=fglobal)
-    else:
-        database=""
+    if not fdatabase: fdatabase=""
+    if not fglobal: fglobal=""
+    if fdatabase or fglobal:
+        iglobals = iglobals.filter(database__icontains=fdatabase).filter(name__icontains=fglobal)
+    if type(fsize) != type(None):
+        fsize = float(fsize)
+        if fsize>=0:
+            iglobals = iglobals.filter(realsize__gte=fsize)
+        else:
+            iglobals = iglobals.filter(realsize__lte=-fsize)
 
-    return render(request, "index.html", {"iglobals": iglobals, "database": database})#, "fglobal":fglobal})
+    return render(request, "index.html", {"iglobals": iglobals, "fdatabase": fdatabase, "fglobal":fglobal, "fsize": fsize})
 
 
 def update(request):
