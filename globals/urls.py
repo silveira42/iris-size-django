0b99e07f6f5c41596fcf291307971b77e80549f4
heloisatambara <heloisatambara@usp.br>
2023-05-22 16:47:03 -0300

    export working

--- globals/urls.py
+++ globals/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import home, update
+from .views import home, update, export
 
 urlpatterns = [
     path('', home, name="home"),
     path('update', update, name="update"),
+    path('export', export, name="export"),
 ]
--- globals/views.py
+++ globals/views.py
@@ -1,33 +1,44 @@
 from django.shortcuts import render, redirect
+from django.db.models import Q
 from .models import iGlobal
 from .api.methods import *
 
 
-# Create your views here.
-def home(request):
+def handle_filters(request):
     iglobals = iGlobal.objects.all()
     fdatabase, fglobal, fsize, fallocated = request.GET.get("fdatabase"), request.GET.get("fglobal"), request.GET.get("fsize"), request.GET.get("fallocated")
 
     if not fdatabase: fdatabase=""
     if not fglobal: fglobal=""
 
-    if fdatabase or fglobal:
-        iglobals = iglobals.filter(database__icontains=fdatabase).filter(name__icontains=fglobal)
+    q1 = Q(database__icontains=fdatabase)
+    q2 = Q(name__icontains=fglobal)
 
     if fsize:
         fsize = float(fsize)
         if fsize>=0:
-            iglobals = iglobals.filter(realsize__gte=fsize)
+            q3 = Q(realsize__gte=fsize)
         else:
-            iglobals = iglobals.filter(realsize__lte=-fsize)
+            q3 = Q(realsize__lte=-fsize)
+    else:
+        q3 = Q(realsize__gte=0)
 
     if fallocated:
         fallocated = float(fallocated)
         if fallocated>=0:
-            iglobals = iglobals.filter(allocatedsize__gte=fallocated)
+            q4 = Q(allocatedsize__gte=fallocated)
         else:
-            iglobals = iglobals.filter(allocatedsize__lte=-fallocated)
+            q4 = Q(allocatedsize__lte=-fallocated)
+    else:
+        q4 = Q(allocatedsize__gte=0)
 
+    iglobals = iglobals.filter(q1, q2, q3, q4)
+
+    return iglobals, fdatabase, fglobal, fsize, fallocated
+
+# Create your views here.
+def home(request):
+    iglobals, fdatabase, fglobal, fsize, fallocated = handle_filters(request)
     return render(request, "index.html", {"iglobals": iglobals, "fdatabase": fdatabase, "fglobal":fglobal, "fsize": fsize, "fallocated": fallocated})
 
 
@@ -41,5 +52,16 @@ def update(request):
         for glob in globalList:
             used, allocated = getGlobalSize(database, glob)
             iGlobal.objects.create(database=database, name=glob, realsize=used, allocatedsize=allocated)
+
+    return redirect(home)
+
+import os
+def export(request):
+    iglobals = handle_filters(request)[0]
+    cd = os.getcwd()
+    with open(cd+"\\test.csv", "w") as file:
+        for iglobal in iglobals:
+            row = iglobal.database+", "+iglobal.name+", "+str(iglobal.realsize)+", "+str(iglobal.allocatedsize)+"\n"
+            file.write(row)
 
     return redirect(home)
